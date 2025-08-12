561133098cb82d409e8999b980741d843d30ae92
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 10:09:26 +0530

    Lecture 54

---
+++ Lecture 54/script.js
@@ -0,0 +1,14 @@
+alert("Hello World");
+
+console.log("Code is running")
+console.log("Code is looking like a wow")
+var a = prompt ("Enter your number")
+console.log("Your number is" + a)
+var is_true = confirm("Are u sure u want to blast the cp")
+if (is_true){
+    console.log("Done with blast")
+} else{
+    console.log("Not successful")
+}
+document.title = "Hey I am script.js"
+document.body.style.backgroundColor = "red"
