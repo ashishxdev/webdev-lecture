6b265018d9f29cfecd724a4c3d8c5e1dfbc9a6c2
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 15:32:31 +0530

    Lecture 60

---
+++ Lecture 60/script.js
@@ -0,0 +1,55 @@
+console.log("This is Lecture 60")
+// let a = "Aashish"
+// console.log(a[0]);
+// console.log(a[1]);
+// console.log(a[2]);
+// console.log(a[3]);
+// console.log(a[4]);
+// console.log(a[5]);
+// console.log(a[6]);
+// // console.log(a[7]);
+// console.log(a.length)
+
+// let ownname = "Aashish"
+// let friend = "Rohan"
+// console.log("His name is " + ownname + " His friend name is " + friend)
+// console.log(`His name is ${ownname} His friend name is ${friend}`)
+
+// let ownname = "Aashish's"
+// console.log(ownname)
+// let escapesequence = "Aashish"s" // Wrong
+// let escapesequence = `Aashish"s`
+// console.log(escapesequence)
+// or
+// let escapesequence = "Aashish\"s"
+// console.log(escapesequence)
+
+// \n, \r, \t for a new line
+// let escapesequence = `Aashish\t"s`
+// let escapesequence = `Aashish\n"s`
+// // let escapesequence = `Aashish\r"s`
+// console.log(escapesequence)
+
+// let b = "Mohan"
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase())
+// console.log(b.length)
+// console.log(b.slice(1,3))// 1 included 3 not included
+// console.log(b.slice(1))// from 1 to end
+// console.log(b.replace("Mo", "34"))
+// let b = "MohanMo"
+// console.log(b.replace("Mo", "34"))
+// In this only starting Mo will be replaced 
+// console.log(b.repeat(3))
+
+// let a = "Aashish";
+// let b = "Mohan";
+// console.log(b.concat(a,"Ram","Sham"))
+
+let c = " Rohan "
+console.log(c.trim(c))
+console.log(c.indexOf("h"))
+console.log(c.indexOf("z"))
+console.log(c.startsWith("z"))
+console.log(c.startsWith(" Ro"))
+console.log(c.endsWith("an "))
