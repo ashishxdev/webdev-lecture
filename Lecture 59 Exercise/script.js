83093517be46ec4860b77fb4646ef774313862ca
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 14:29:57 +0530

    Lecture 59

---
+++ Lecture 59 Exercise/script.js
@@ -0,0 +1,61 @@
+/* Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+
+It performs wrong operation 10% of the times
+
+*/
+
+if (Math.random() < 0.1){
+function sum(a,b){
+    console.log(a - b);
+} 
+function min(a,b){
+    console.log(a / b);
+}
+function mult(a,b){
+    console.log(a - b);
+}
+function div(a,b){
+    console.log(a ** b);
+}
+function power(a,b){
+    console.log(a ** b);
+}
+function mod(a,b){
+    console.log(a % b);
+}
+} else {
+function sum(a,b){
+    console.log(a + b);
+} 
+function min(a,b){
+    console.log(a - b);
+}
+function mult(a,b){
+    console.log(a * b);
+}
+function div(a,b){
+    console.log(a / b);
+}
+function power(a,b){
+    console.log(a ** b);
+}
+function mod(a,b){
+    console.log(a % b);
+} 
+}
+
+sum(8,2)
+min(8,2)
+mult(8,2)
+div(8,2)
+mod(8,2)
