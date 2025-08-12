6b5a7f85975235c814d359cbd11e3eb01ce6ace8
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 12:09:39 +0530

    Lecture 56

---
+++ Lecture 56/script.js
@@ -0,0 +1,91 @@
+console.log("Hello I am conditional tutorial")
+
+// if else condition statement
+// let age = 45;
+// if (age > 19){
+//     console.log("You can drive")
+// }
+// else{
+// console.log("You are not eligible")
+// }
+
+// arithmetic operators
+// let age = 3;
+// let grace = 2;
+
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace) // 3^2 = 3*3 = 9
+// console.log(age % grace)
+
+// if ((age+grace) > 18){
+//     console.log("You can drive")
+// }
+// else{
+// console.log("You are not eligible")
+// }
+
+// assignment operators
+// let age = 3;
+// let grace = 2;
+// age += grace;
+// console.log(age)
+// console.log(age -= grace)
+
+// if (age > 18){
+//     console.log("You can drive")
+// }
+// else{
+// console.log("You are not eligible")
+// }
+
+// comparison operator & if else if else ladder
+// let age = 3;
+// let grace = 2;
+// if (age == 18) {
+//     console.log("You can drive");
+// }
+
+// else if (age == 0) {
+//     console.log("Are you kidding?")
+// }
+
+// else if (age == 1) {
+//     console.log("Are you again kidding?")
+// }
+
+// else {
+//     console.log("You cannot drive");
+// }
+
+// logical operator
+// let a = 5;
+// let b = 7;
+// console.log (a != b)
+// console.log (a!=b && b==a)
+// console.log (a!=b || b==a)
+
+/*
+I am a 
+multiline 
+comment
+*/
+// I am a single line comment
+
+// ternary operator
+a = 6;
+b = 8;
+let c = a > b ? (a - b) : (b - a);
+
+/*
+translates to:
+if(a>b){
+    let c = a - b;
+}
+else {
+    let c = a - b;
+}
+
+*/
