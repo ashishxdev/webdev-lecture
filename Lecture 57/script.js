99711c2ba31cbb78fafdd8d6f9a423c53a6d0e99
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 13:10:20 +0530

    Lecture 57

---
+++ Lecture 57/script.js
@@ -0,0 +1,36 @@
+// let a = 3; 
+// for (let o = 4; o < 10; o++){
+//     console.log(o); 
+// }
+// for (let o = 4; o < 10; o++){
+//     console.log(a + o); 
+// }
+
+// let obj = {
+//     name:"Aashish",
+//     programmer:"yes",
+//     company:"AI"
+// }
+// // for in loop
+// for (const key in obj) { 
+//     console.log(key)
+// }
+
+// for of loop
+// for (const c of "Aashish") { 
+//     console.log(c)
+// }
+
+// while loop
+// let a = 2;
+// while (a < 10){
+//     console.log(a);
+//     a++;
+// }
+
+// do while loop
+let a = 9;
+do{
+    console.log(a);
+    a++;
+} while (a < 8)
