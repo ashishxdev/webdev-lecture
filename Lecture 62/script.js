f02c9ddb66b6708d93e651474f162e301ca30b67
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 15:58:04 +0530

    Lecture 62

---
+++ Lecture 62/script.js
@@ -0,0 +1,30 @@
+/* Create a business name generator by combining list of adjectives and shop name and another word
+
+Adjectives:
+Crazy 
+Amazing
+Fire 
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+*/
+function businessname(x,y,z){
+    let num = Math.floor(Math.random()*3)
+    if (num==0){
+        return x;
+    } else if (num==1){
+        return y;
+    }
+    else {
+        return z;
+    }
+}
+console.log(`Generated name is ${businessname("crazy","amazing","fire")} ${businessname("engine","foods","garments")} ${businessname("bros","limited","hub")} `)
