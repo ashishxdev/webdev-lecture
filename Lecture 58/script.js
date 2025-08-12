785da89f1bb937ca57e2f3f2887b9f99380fdb31
ashishxdev <ashishrana2004@gmail.com>
2025-08-12 13:40:37 +0530

    Lecture 58

---
+++ Lecture 58/script.js
@@ -0,0 +1,37 @@
+// function awesome(naam){
+//     console.log("Hello " + naam + " you are awesome")
+//     console.log("Hello " + naam + " you are a nice guy")
+//     console.log("Hello " + naam + " you are looser")
+//     console.log("Hello " + naam + " you are successfull")
+// }
+// awesome("Aashish");
+// awesome("Mohan")
+
+// function sum(a, b){
+//     console.log(a + b);
+// }
+// sum(2,5)
+// sum(3,5)
+// sum(4,5)
+
+// function sum(a, b, c = 2){
+//     // console.log(a + b);
+//     console.log(a, b, c);
+//     return a + b + c;
+// }
+// result = sum(4)
+// result1 = sum(4, 5)
+// result2 = sum(3, 5)
+// result3 = sum(1, 5, 20)
+// console.log("The sum of a and b is:",result)
+// console.log("The sum of a and b is:",result1)
+// console.log("The sum of a and b is:",result2)
+// console.log("The sum of a and b is:",result3)
+
+// arrow function
+// const func1 = (x) =>{
+//     console.log("This is an arrow function", x)
+// }
+// func1(34)
+// func1(50)
+// func1(44)
