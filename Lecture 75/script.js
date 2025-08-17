b1968bf669bba93a88f142eb78cb3ad20c39334a
ashishxdev <ashishrana2004@gmail.com>
2025-08-17 23:17:12 +0530

    Lecture 75

---
+++ Lecture 75/script.js
@@ -0,0 +1,32 @@
+// asynchronous actions
+// console.log("Aashish is a hacker")
+// console.log("Shumbham is a hucker")
+
+// setTimeout(() => {
+//     console.log("I am inside settimeout")
+// }, 0);
+
+// setTimeout(() => {
+//     console.log("I am inside settimeout 2")
+// }, 1000);
+
+// console.log("The End")
+
+// callback function
+const fn = () => {
+  console.log("Nothing")
+}
+
+const callback = (arg,fn) => {
+    console.log(arg)
+    fn()
+}
+
+const loadScript = (src, callback) => {
+    let sc = document.createElement("script");
+    sc.src = src;
+    sc.onload = callback("Popcorn",fn);
+    document.head.append(sc)
+}
+
+loadScript("https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js", callback )
