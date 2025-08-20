67453eff6579696b090cc78988b5c1d7466f4559
ashishxdev <ashishrana2004@gmail.com>
2025-08-20 21:00:27 +0530

    L82+83

---
+++ Lecture 82/script.js
@@ -0,0 +1,47 @@
+// async function sleep(){
+//     return new Promise((resolve, reject)=>{
+//         setTimeout(() => {
+//             resolve(45)
+//         }, 1000);
+//     })
+// }
+// // let a = await sleep()
+// // let b = await sleep()
+// // it will show an error
+
+// // so we use iife that's creating a function
+// (async function main(){
+//     let a = await sleep()
+//     console.log(a)
+//     let b = await sleep()
+//     console.log(b)
+// })()
+
+// Destructuring
+// (async function main(){
+//     // let [x,y, ...rest] = [1, 5, 7, 8, 9] // x = 1, y = 5
+//     // console.log(x, y, rest)
+
+//     let obj = {
+//         a: 3,
+//         b: 4, 
+//         c: 5
+//     }
+
+//     let {a, b} = obj
+//     console.log(a, b) // a = 3; b = 4;
+// })()
+
+// Spread syntax
+// function sum(a, b, c){
+//     return a+ b + c
+// }
+// let arr = [1, 4, 6]
+// console.log(sum(...arr)) // ... spread operator
+// // console.log(arr[0] + arr[1] + arr[2])
+// // console.log(sum(arr[0], arr[1], arr[2]))
+
+// Hosting
+console.log(a1);
+var a1 = 6; // undefined - moves the declaration at the top of the code before execution
+// let a1 = 6; // shows error
