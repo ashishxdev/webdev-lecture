9a710ad51420148c53bfdd2938d2f8cbb0adcae5
ashishxdev <ashishrana2004@gmail.com>
2025-08-31 17:49:26 +0530

    L70

--- Lecture 70 Exercise/script.js
+++ Lecture 70 Exercise/script.js
@@ -1,9 +1,5 @@
 console.log("Assigning Random Colors to boxes")
 
-// Array.from(document.getElementsByTagName("div")).forEach(element => {
-//     element.style.backgroundColor = "cyan"
-// });
-
 document.querySelectorAll(".box").forEach(e=>{
     console.log(r = Math.floor(Math.random()*256))
     console.log(g = Math.floor(Math.random()*256))
