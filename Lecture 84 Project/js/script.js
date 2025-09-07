539be2d749acf4972599eb60b6071754c3683007
ashishxdev <ashishrana2004@gmail.com>
2025-09-07 13:09:58 +0530

    updated

--- Lecture 84 Project/js/script.js
+++ Lecture 84 Project/js/script.js
@@ -72,7 +72,7 @@ const playmusic = (track, pause = false) => {
 
 }
 async function displayAlbums(){
-    let a = await fetch(`/songs/`)
+    let a = await fetch(`http://127.0.0.1:3000/songs/`)
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
