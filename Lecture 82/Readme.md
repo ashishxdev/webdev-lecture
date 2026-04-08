ca2f7e295abebdf62695962432b164ac8a5d5935
ashishxdev <ashishrana2004@gmail.com>
2026-04-08 23:45:08 +0530

    L82

---
+++ Lecture 82/Readme.md
@@ -0,0 +1,8 @@
+- An IIFE is a function that runs immediately after being defined, used to create a private scope and avoid global variables
+  (function(name){
+  console.log("Hello " + name);
+  })("Aashish");
+- Destructuring
+- Spread Syntax
+- Local, Global and Block Scope
+- Hoisting
--- Lecture 82/script.js
+++ Lecture 82/script.js
@@ -1,3 +1,15 @@
+// iife
+// (function (name) {
+//     console.log("Hello " + name);
+// })("Aashish");
+
+// let x = 10;
+// (function(){
+//     let x = 20;
+//     console.log(x);
+// })();
+// console.log(x);
+
 // async function sleep(){
 //     return new Promise((resolve, reject)=>{
 //         setTimeout(() => {
@@ -7,9 +19,9 @@
 // }
 // let a = await sleep()
 // let b = await sleep()
-// // it will show an error
+// it will show an error
 
-// // so we use iife that's creating a function
+// so we use iife that's creating a function
 // (async function main(){
 //     let a = await sleep()
 //     console.log(a)
@@ -18,31 +30,41 @@
 // })()
 
 // Destructuring
-// (async function main(){
-//     // let [x,y, ...rest] = [1, 5, 7, 8, 9] // x = 1, y = 5, rest = rest
-//     // console.log(x, y, rest)
+// (async function main() {
+//     let [x, y, ...rest] = [1, 5, 7, 8, 9] // x = 1, y = 5, rest = rest
+//     console.log(x, y, rest)
 
 //     let obj = {
 //         a: 3,
-//         b: 4, 
+//         b: 4,
 //         c: 5
 //     }
 
-//     let {a, b, c} = obj
+//     let { a, b, c } = obj
 //     console.log(obj);
 //     console.log(a, b, c) // a = 3; b = 4; c = 5;
 // })()
 
+// const a = "the", b = "no"
+// const c = { a, b }
+// console.log(c)
+
 // Spread syntax
-// function sum(a, b, c){
+// function sum(a, b, c) {
 //     return a + b + c
 // }
 // let arr = [1, 4, 6]
 // console.log(sum(...arr)) // ... spread operator
-// // console.log(arr[0] + arr[1] + arr[2])
-// // console.log(sum(arr[0], arr[1], arr[2]))
+// console.log(arr[0] + arr[1] + arr[2])
+// console.log(sum(arr[0], arr[1], arr[2]))
 
-// Hosting
-console.log(a1);
-var a1 = 6; // undefined - moves the declaration at the top of the code before execution
+// Hoisting
+// hello("Aashish")
+// function hello(name){
+//     console.log("Hello " + name)
+// }
+
+// console.log(a1);
+// var a1 = 6; // undefined
 // let a1 = 6; // shows error
+
